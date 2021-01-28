6a99498a7cd59367d63b498c5caa658bfb06cf75
Spartak <spartak.virabyan.2005@mail.ru>
2021-01-28 10:36:54 +0400

    added LocalData

--- src/main/Shop.java
+++ src/main/Shop.java
@@ -1,4 +1,28 @@
 package main;
 
+import java.util.Scanner;
+
 public class Shop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        printSections();
+        int section = scanner.nextInt();
+        switch (section){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+
+        }
+    }
+    public static void printSections(){
+        System.out.println("Բարի գալուստ <<ՀԱՄԱԿԱՐԳԻՉՆԵՐԻ ԱՇԽԱՐՀ>>"+"\n"+
+                "============   Բաժիններ  ============"+"\n"+
+                "1. Համակարգիչներ"+"\n"+
+                "2․ Զամբյուղ"+"\n"+
+                "1. Հետադարձ կապ"+"\n"+
+                " Ընտրեք բաժինը (Օր․՝ 1 + Enter)"+"\n");
+    }
 }
